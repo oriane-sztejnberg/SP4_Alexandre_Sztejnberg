886f4e7debd97acd943e477a73a414461acae54b
jason <jason.alexandre@epfedu.fr>
2022-11-30 17:07:46 +0100

    fin de la realisation de la classe Plateau de Jeu avec l'ensemble des methodes.

--- src/sp4_alexandre_sztejnberg/Plateaudejeu.java
+++ src/sp4_alexandre_sztejnberg/Plateaudejeu.java
@@ -96,7 +96,114 @@ public class Plateaudejeu {
         }
         return false;   
     }
-    
-    
-    
+    public boolean diagonaleMontanteGagnantePourCouleur(String couleur){
+        int compteur=0;
+        for (int i=0; i<3;i++){
+            for(int j=0; j<4;j++){
+                for (int k=0;k<7;k++){
+                    if (grille[i+k][j+k].lireCouleurDuJeton()==couleur){
+                        compteur+=1;
+                    }else{
+                        compteur=0;
+                }
+                    if(compteur==4){
+                        return true;
+                    }
+                    if (i==5 && j==6){
+                        break;
+                    }
+                }
+            compteur=0;
             }
+        }
+        return false;
+    }
+    public boolean diagonaleDescendanteGagnantePourCouleur(String couleur){
+        int compteur=0;
+        for(int i=3; i<6; i++){
+            for (int j=0;j<4;j++){
+                for(int k=0; k<7; k++){
+                     if (grille[i-k][j+k].lireCouleurDuJeton()==couleur){
+                    compteur+=1;
+                    }   
+                    else {
+                        compteur=0;
+                    }if (compteur==4){
+                        return true;
+                    }
+                    if (i==0&&j==6){
+                        break;
+                 
+                    }
+                }  
+            
+            compteur=0;
+    
+        }
+    }
+    return false;
+    
+    }
+public void tasserLigne (int colonne){
+    for (int i=0; i<6;i++){
+        if (grille[i][colonne]==null){
+            for (int j=i;j<5;j++){
+                grille [j][colonne]=grille[j+1][colonne];    
+            }
+            grille[5][colonne]=null;
+        }
+    }
+}
+public boolean colonneRemplie(int colonne){
+    return grille[5][colonne]!=null;
+    
+}
+
+public boolean placerTrouNoir(int ligne, int colonne){
+    if (grille[ligne][colonne].presenceTrouNoir()==true){
+        grille[ligne][colonne].placerTrouNoir();
+        return true;
+    }else{
+        return false;
+    }
+    
+}
+public boolean supprimerTrouNoir(int ligne, int colonne){
+    if (grille[ligne][colonne].presenceTrouNoir()==true){
+        grille[ligne][colonne].supprimerTrouNoir();
+        return true;
+    }else{
+        return false;
+    }
+}
+public boolean placerDesintegrateur(int ligne, int colonne){
+    if (grille[ligne][colonne].presenceDesintegrateur()==true){
+        grille[ligne][colonne].placerDesintegrateur();
+        return true;
+    }else{
+        return false;
+    }
+}
+public boolean supprimerDesintegrateur(int ligne, int colonne){
+    if (grille[ligne][colonne].presenceDesintegrateur()==true){
+        grille[ligne][colonne].supprimerDesintegrateur();
+        return true;
+    }else{
+        return false;
+    }
+}
+public boolean supprimerJeton(int ligne, int colonne){
+    if(grille[ligne][colonne].presenceJeton()==true){
+        grille[ligne][colonne].supprimerJeton();
+        return true;
+    }else{
+        return false;
+    }
+}
+public Jeton recupererJeton(int ligne,int colonne){
+    return grille[ligne][colonne].recupererJeton();
+}
+
+}
+
+
