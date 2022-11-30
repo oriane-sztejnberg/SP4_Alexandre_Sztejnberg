8193e17c2ed48866dcfe7475c09c2fb69c126c16
jason <jason.alexandre@epfedu.fr>
2022-11-30 15:02:15 +0100

    creation des methodes du plateau de jeu, on regarde si il y a 4 jetons presents sur la colonne et s'ils sont de la même couleur. dans ce cas la colonne sera gagnante.

--- src/sp4_alexandre_sztejnberg/Plateaudejeu.java
+++ src/sp4_alexandre_sztejnberg/Plateaudejeu.java
@@ -9,30 +9,94 @@ package sp4_alexandre_sztejnberg;
  * @author jason
  */
 public class Plateaudejeu {
+
     CelluleDeGrille[][] grille = new CelluleDeGrille[6][7];
-    
-    public Plateau(){
-        for (int i=0;i<6;i++){
-            for (int j =0;j<7;j++){
-                grille[i][j]=new CelluleDeGrille();
+
+    public void PlateauDeJeu() {
+        for (int i = 0; i < 6; i++) {
+            for (int j = 0; j < 7; j++) {
+                grille[i][j] = new CelluleDeGrille();
             }
         }
     }
-    public int ajouterJetonDansColonne(Jeton UnJeton,int colonne){
-        for (int k=5;k>=0;k--){
-            if("Rouge".equals(grille[k][colonne].lireCouleurDuJeton())||"Jaune".equals(grille[k][colonne])){
-                grille[k+1][colonne].affecterJeton(UnJeton);
-                return k+1;
+
+    public int ajouterJetonDansColonne(Jeton UnJeton, int colonne) {
+        for (int k = 5; k >= 0; k--) {
+            if ("Rouge".equals(grille[k][colonne].lireCouleurDuJeton()) || "Jaune".equals(grille[k][colonne])) {
+                grille[k + 1][colonne].affecterJeton(UnJeton);
+                return k + 1;
+            }
+        }
+        return 0;
+    }
+
+    public boolean grilleRemplie() {
+        for (int k = 0; k <= 6; k++) {
+            if (grille[5][k].presenceJeton() == true) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public void afficherGrilleSurConsole() {
+        for (int i = 0; i < 6; i++) {
+            for (int j = 0; j < 7; j++) {
+                if (j == 6) {
+                    System.out.print(grille[i][j] + "/n");
+                } else {
+                    System.out.print(grille[i][j] + "");
                 }
             }
-            return 0;   
-            }
-    public boolean grilleRemplie(){
-        for (int k=0;k<=6;k++){
-            if (grille[5][k].presenceJeton()==true){
-                return true;
-            } 
         }
-        return false; 
     }
-}
+
+    public boolean presenceJeton(int ligne, int colonne) {
+        return grille[ligne][colonne].presenceJeton() == true;
+    }
+
+    public String lireCouleurDuJeton(int ligne, int colonne) {
+        return grille[ligne][colonne].lireCouleurDuJeton();
+    }
+
+    public boolean LigneGagnantePourCouleur(String couleur) {
+        int compteur = 0;
+        for (int i = 0; i < 6; i++) {
+            for (int j = 0; j < 7;j++){
+                if (grille[i][i].lireCouleurDuJeton()==couleur){
+                    compteur += 1;
+                    if (compteur== 4) {
+                        return true;
+
+                    }
+                }else{
+                    compteur=0;
+                }
+                
+            }
+            compteur=0;
+        }
+        return false;
+    }
+    
+    public boolean colonneGagnantePourCouleur(String couleur){
+        int compteur=0;
+        for (int j=0; j<7; j++){
+            for (int i=0; i<6; i++){
+                if (grille[i][j].lireCouleurDuJeton()==couleur){
+                    compteur+=1;
+                    if (compteur==4){
+                        return true;
+                    }
+                }else{
+                    compteur=0;
+                }
+            }
+            compteur=0;
+        }
+        return false;   
+    }
+    
+    
+    
+            }
